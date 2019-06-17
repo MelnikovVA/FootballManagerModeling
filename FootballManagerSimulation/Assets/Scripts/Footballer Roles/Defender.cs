2e02d6e84397db1254a4b99f38cf88379ac3ab5a
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-18 03:25:56 +0700

    Added Defender class
    No additional fields

---
+++ FootballManagerSimulation/Assets/Scripts/Footballer Roles/Defender.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public class Defender : MonoBehaviour, IPlayer
+    {
+        public static System.Random rand = new System.Random();
+        public string Name { get; set; }
+        public Role Role { get; set; }
+        public double Attack { get; set; }
+        public double Defense { get; set; }
+        public double StrikePrecision;
+
+        public Defender(string name)
+        {
+            this.Name = name;
+            this.Role = Role.Defender;
+            this.Attack = rand.Next(5, 20);
+            this.Defense = rand.Next(30, 50);
+        }
+    }
+}
