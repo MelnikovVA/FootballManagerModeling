8ea2e23a71a0cb3f2454e70fad21153157f255bb
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-18 02:47:52 +0700

    Added IPlayer interface
    Basic representation of every player's parameters. There will be three inheritors: Goalkeeper, Forward and Defender

---
+++ FootballManagerSimulation/Assets/Scripts/Footballer Roles/IPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public interface IPlayer
+    {
+        string Name { get; set; }
+        string Role { get; set; }
+        double Attack { get; set; }
+        double Defense { get; set; }
+    }
+}
