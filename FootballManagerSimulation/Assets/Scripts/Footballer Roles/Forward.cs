621617574b5cb834f6409eea817416540a5e1116
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-18 03:15:31 +0700

    Added Forward class
    One additional field compared to the interface: StrikePrecision

---
+++ FootballManagerSimulation/Assets/Scripts/Footballer Roles/Forward.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public class Forward : MonoBehaviour, IPlayer
+    {
+        public static System.Random rand = new System.Random();
+        public string Name { get; set; }
+        public Role Role { get; set; }
+        public double Attack { get; set; }
+        public double Defense { get; set; }
+        public double StrikePrecision;
+
+        public Forward(string name)
+        {
+            this.Name = name;
+            this.Role = Role.Forward;
+            this.Attack = rand.Next(50, 80);
+            this.Defense = rand.Next(0, 20);
+            this.StrikePrecision = rand.NextDouble();
+
+            if (StrikePrecision < 0.5)
+                StrikePrecision += 0.5;
+        }
+    }
+}
