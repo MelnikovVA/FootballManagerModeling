b4de3f09f9af7141c1857486ece4543a1defcc2b
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-18 01:19:46 +0700

    Added DataProvider class
    This class contains players' first and last names that later will be used for team creation

---
+++ FootballManagerSimulation/Assets/Scripts/DataProvider.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataProvider : MonoBehaviour
+{
+    public static List<string> PlayersBelgium = new List<string> { "Marijn Demir", "Benjamin Huijbrechts", "Dani Miner",
+        "Rayan Marchal", "Julius van de Brink", "Maarten van de Velden", "Sep de Haan", "Cas Hussain", "Bryan Hermans",
+        "Faas Blokland", "Rik Weijland" };
+
+    public static List<string> PlayersBrazil = new List<string> { "Djalma Santos", "Gilberto Silva", "Thiago Silva",
+        "Toninho Cerezo", "Carlos Alberto", "Hilderaldo Bellini", "Roque Junior", "Ricardo Gomes", "Luiz Gustavo",
+        "Joel Camargo", "Philippe Coutinho"};
+
+    public static List<string> PlayersEngland = new List<string> { "Dan James", "Connor Murray", "Finley Hunt",
+        "Tim Campbell", "Christian Hughes", "Andy Anderson", "Mark Rogers", "Scott Price", "Jackson Wilkinson",
+        "Zachary Ward", "Greg Parker" };
+
+    public static List<string> PlayersFrance = new List<string> { "Marcel Texier", "Julien Valette", "Isaac Poulain",
+        "Victor Philippe", "Xavier Jourdan", "Antoine Dijoux", "Dominique Rossi", "Laurent Weber", "Franck Diallo",
+        "Alphonse Benoit", "Roland Rousset" };
+
+    public static List<string> PlayersGermany = new List<string> { "Eberhard Wilhelm", "Ewald Wegener", "Hugo Wagner",
+        "Willy Appel", "Markus Hagen", "Falko Scherer", "Helmuth Hummel", "Cornelius Brunner", "Dietmar Schott",
+        "Heinz Noll", "Jakob Bischoff" };
+
+    public static List<string> PlayersPortugal = new List<string> { "Fernando Couto", "Rui Costa", "Ricardo Carvalho",
+    "Nuno Gomes", "	Raul Meireles", "Humberto Coelho", "Paulo Ferreira", "Hugo Almeida",
+    "Fernando Meira", "Paulo Sousa", "Carlos Manuel", };
+
+    public static List<string> PlayersRussia = new List<string> { "Oleg Lazarev", "Vlad Kondyrev", "Leonid Goryaynov",
+        "Aleksei Karageorgi", "Aleksei Gonchar", "Danil Grezev", "Alisher Murzagulov", "Alexander Yudin", "Danil Babenkov",
+        "Igor Romin", "Denis Sharapov", "Oleg Abramov", "Oleg Pavlovich", "Andrew Shokolov" };
+
+    public static List<string> PlayersSpain = new List<string> { "Angel Valladares", "Rodrigo Aguirre", "Lucas Viera",
+        "Antonio Santos", "Gonzalo Cuenca", "Pablo Conde", "Eduardo Montalvo", "Andres Robledo",
+        "Santiago Calvillo", "Fernando Atencio", "Aitor Zepeda" };
+}
