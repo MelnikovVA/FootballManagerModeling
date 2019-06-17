b8c2c27ba6d738d4da73a792bbbd12db0842db7c
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-18 03:22:51 +0700

    Added Goalkeeper class
    Two additional fields: GoalProtection and ChanceOfSave

---
+++ FootballManagerSimulation/Assets/Scripts/Footballer Roles/Goalkeeper.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public class Goalkeeper : MonoBehaviour, IPlayer
+    {
+        public static System.Random rand = new System.Random();
+        public string Name { get; set; }
+        public Role Role { get; set; }
+        public double Attack { get; set; }
+        public double Defense { get; set; }
+        public double GoalProtection;
+        public double ChanceOfSave;
+
+        public Goalkeeper(string name)
+        {
+            this.Name = name;
+            this.Role = Role.Goalkeeper;
+            this.Attack = rand.Next(0, 10);
+            this.Defense = 0;
+            this.GoalProtection = rand.Next(20, 35);
+            this.ChanceOfSave = GoalProtection / 100;
+        }
+    }
+}
