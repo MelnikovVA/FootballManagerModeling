0ee05c1eda90c8c29bc2a1f3b4ab3e90d75070c4
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-29 01:24:27 +0700

    Started making GameManager
    This whole class is for handling game logic.
    Currently has just a few fields.
    There will be at least two #region's - first is for holding a match, second is for training a team.

---
+++ FootballManagerSimulation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager GM;
+        public static Random Rand = new Random();
+
+        public List<FootballTeam> Teams = new List<FootballTeam>();
+        public List<Match> Matches = new List<Match>();
+
+
+        private void Awake()
+        {
+            GM = this;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        void CreateTeams()
+        {
+            for (int i=0; i<8; i++)
+            {
+                Teams.Add(new FootballTeam(i));
+            }
+        }
+
+       
+        
+    }
+    #region Playing a match
+    #endregion
+}
