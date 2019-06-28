20f4b7835a77a043b5037a139fe4bf86a9724855
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-28 23:10:49 +0700

    Implemented Goal class
    Inherited from IBallEvent

---
+++ FootballManagerSimulation/Assets/Scripts/BallEvents/Goal.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public class Goal : IBallEvent
+    {
+        public int EventTime { get; set; }
+        public IPlayer Player { get; set; }
+
+        Goal(int time, Forward forward)
+        {
+            this.EventTime = time;
+            this.Player = forward;
+        }
+    }
+}
