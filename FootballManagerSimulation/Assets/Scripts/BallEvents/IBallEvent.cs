063ab5dbe163bd04c642d67525310c432d01fe50
Vladimir Melnikov <vladmeln158@yandex.ru>
2019-06-28 23:01:48 +0700

    Made IBallEvent interface
    Need to create two children for goals and saves

---
+++ FootballManagerSimulation/Assets/Scripts/BallEvents/IBallEvent.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FootballManagerModeling
+{
+    public interface IBallEvent
+    {
+        int EventTime { get; set; }
+        IPlayer Player { get; set; }
+    }
+}
